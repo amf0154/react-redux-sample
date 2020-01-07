f6b7a9ae717cd27083c83acc0c58a4c390d96389
Gleb Bykovsky <gleb@bykovsky.dev>
2020-01-07 21:33:26 +0300

    feat: form exmaple

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter,Switch, Route} from 'react-router-dom';
 import Questions from './components/Questions';
 import { Provider } from 'react-redux';
 import Home from './components/Home';
+import Form from './components/Form';
 import store from './store/store';
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Provider store={store}>
       <Route path='/home' component={Home} />
       <Route path='/main' component={Questions} />
+      <Route path='/form' component={Form} />
       </Provider>
       </Switch>
     </BrowserRouter>
---
+++ src/components/Form.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {connect} from 'react-redux';
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isGoing: true,
+          numberOfGuests: 2
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+      }
+    
+    handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+        [name]: value
+    });
+    setTimeout(()=>console.log(this.state), 300); 
+    }
+    
+    render() {
+        return (
+            <form>
+            <label>
+                Пойду:
+                <input
+                name="isGoing"
+                type="checkbox"
+                checked={this.state.isGoing}
+                onChange={this.handleInputChange} />
+            </label>
+            <br />
+            <label>
+                Количество гостей:
+                <input
+                name="numberOfGuests"
+                type="number"
+                value={this.state.numberOfGuests}
+                onChange={this.handleInputChange} />
+            </label>
+            </form>
+        );
+    }
+}
+function mapStateToProps(state){
+    return {
+      data: state
+    }
+}
+export default connect(mapStateToProps)(Form);
